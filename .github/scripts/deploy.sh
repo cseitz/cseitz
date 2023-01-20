a212a67e9ab8c0a8537e46e40077def3e57f4410
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2023-01-20 13:05:46 -0500

    a

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -3,7 +3,7 @@
 # Environment Variables
 CWD=$( pwd );
 REPO=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && git rev-parse --show-toplevel );
-PM2_PROJECT=$PM2_PREFIX-$PROJECT
+# PM2_PROJECT=$PM2_PREFIX-$PROJECT
 
 echo "building $PROJECT"
 BUILD_DIR=.next-build npm run build -- --only -vvv --filter=$PROJECT
@@ -19,16 +19,16 @@ if [ -d ".next-build" ]; then
     # Copy over the new one
     if mv .next-build .next; then
     #   Start the project
-        if pm2 show $PM2_PROJECT; then
-            echo "pm2: start $PM2_PROJECT"
-            NODE_ENV=production pm2 restart $PM2_PROJECT --update-env
+        if pm2 show $PM2_NAME; then
+            echo "pm2: start $PM2_NAME"
+            NODE_ENV=production pm2 restart $PM2_NAME --update-env
         else
-            echo "pm2: initialize $PM2_PROJECT"
+            echo "pm2: initialize $PM2_NAME"
             cd $REPO
-            pm2 start --name $PM2_PROJECT "npm run start -- --only --filter=$PROJECT"
+            pm2 start --name $PM2_NAME "npm run start -- --only --filter=$PROJECT"
             cd $PWD
         fi
-        pm2 show $PM2_PROJECT
+        pm2 show $PM2_NAME
         rm -rf .next-old
     else
         mv .next-old .next
