e0c971413282b17af84cd607baa59cead6ef881d
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2023-01-20 14:32:31 -0500

    a

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -3,6 +3,6 @@ import { Box } from '@mantine/core';
 
 export default function Homepage() {
     return <Box>
-        Chris Seitz Personal Website2
+        Chris Seitz Personal Website3
     </Box>
 }
